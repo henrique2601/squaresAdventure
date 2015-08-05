85b9b95d3db160b0a8663fee17c050ef0cdc9a8e
Pablo Henrique Bertaco <pablo_fonseca91@icloud.com>
2015-08-05 13:22:39 -0300

    Alterado nome do projeto para TowerUp e adicionados fremewokrs do Parse

---
+++ TowerUp/TowerUp-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  TowerUp-Bridging-Header.h
+//
+//  Copyright 2011-present Parse Inc. All rights reserved.
+//
+
+#ifndef TowerUp_Bridging_Header_h
+#define TowerUp_Bridging_Header_h
+
+// If you are using Facebook, uncomment this line to get automatic import of the header inside your project.
+//#import <ParseFacebookUtils/PFFacebookUtils.h>
+
+#endif
