033ad223edc97b3e96bf68639c75261956074f5a
Pablo Henrique <pablohenrique@MacBook-Pro-de-Eldorado.local>
2015-08-28 09:00:18 -0300

    pablo

---
+++ TowerUp/Boxes/MultiplayerWinBox.swift
@@ -0,0 +1,43 @@
+//
+//  MultiplayerWinBox.swift
+//  TowerUp
+//
+//  Created by Pablo Henrique on 28/08/15.
+//  Copyright (c) 2015 WTFGames. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class MultiplayerWinBox: Box {
+    
+    var labelTime:Label!
+    var labelDeaths:Label!
+    var labelBonus:Label!
+    
+    init(background: String, name:String) {
+        super.init(background: background)
+        
+        self.addChild(Button(name: "buttonExit", textureName: "buttonGraySquare", text:"X", x: 98, y: 590))
+        
+        self.labelTime = Label(name: "labelName", color:GameColors.black, textureName: name, x: 288, y: 226)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func touchesEnded(touches: Set<NSObject>, withEvent event: UIEvent) {
+        super.touchesEnded(touches, withEvent: event)
+        for touch in (touches as! Set<UITouch>) {
+            let location = touch.locationInNode(self)
+            
+            if (self.childNodeWithName("buttonExit")!.containsPoint(location)) {
+                if let scene = self.scene as? MultiplayerGameScene {
+                    scene.view!.presentScene(MainMenuScene(), transition: Config.defaultGoTransition)
+                }
+                return
+            }
+        }
+    }
+}
