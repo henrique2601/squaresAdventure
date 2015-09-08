0a4ec751a68ac9857702528f449728e7ca116528
Pablo Henrique <pablohenrique@MacBook-Pro-de-Eldorado.local>
2015-09-08 16:18:45 -0300

    Correção de bug de física do multiplayerOnline

--- TowerUp/src/World.swift
+++ TowerUp/src/World.swift
@@ -11,6 +11,9 @@ import SpriteKit
 
 class World: SKNode {
     
+    var bodyA: SKPhysicsBody!
+    var bodyB: SKPhysicsBody!
+    
     init(physicsWorld:SKPhysicsWorld) {
         super.init()
         physicsWorld.gravity = CGVector(dx: 0.0, dy: -9.8)
@@ -22,37 +25,64 @@ class World: SKNode {
     
     func didBeginContact(contact: SKPhysicsContact) {
         
-        switch(contact.bodyA.categoryBitMask) {
+        //Assign the two physics bodies so that the one with the lower category is always stored in firstBody
+        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+            self.bodyA = contact.bodyA
+            self.bodyB = contact.bodyB
+        } else {
+            self.bodyA = contact.bodyB
+            self.bodyB = contact.bodyA
+        }
+        
+        switch(self.bodyA.categoryBitMask) {
         case physicsCategory.player.rawValue:
-            (contact.bodyA.node as! Player).didBeginContact(contact.bodyB)
+            (self.bodyA.node as! Player).didBeginContact(self.bodyB)
             break
         default:
-            println("didBeginContact de \(contact.bodyA.node!.name!) não está sendo processado")
+            println("didBeginContact de \(self.bodyA.node?.name) não está sendo processado")
             break
         }
     }
     
     func didEndContact(contact: SKPhysicsContact) {
-        switch(contact.bodyA.categoryBitMask) {
+        
+        //Assign the two physics bodies so that the one with the lower category is always stored in firstBody
+        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+            self.bodyA = contact.bodyA
+            self.bodyB = contact.bodyB
+        } else {
+            self.bodyA = contact.bodyB
+            self.bodyB = contact.bodyA
+        }
+        
+        switch(self.bodyA.categoryBitMask) {
         case physicsCategory.player.rawValue:
-            (contact.bodyA.node as! Player).didEndContact(contact.bodyB)
+            (self.bodyA.node as! Player).didEndContact(self.bodyB)
             break
         default:
-            println("didEndContact de \(contact.bodyA.node!.name!) não está sendo processado")
+            println("didEndContact de \(self.bodyA.node?.name) não está sendo processado")
             break
         }
     }
 }
 
-public enum physicsCategory : UInt32 {
-    case none     = 0
-    case player   = 1
-    case enemy    = 2
-    case bullet   = 4
-    case ground   = 8
-    case winTile  = 16
-    case coin     = 32
-    case spike    = 64
-    case doorTile = 128
-    case all      = 4294967295
+struct physicsCategory : RawOptionSetType {
+    typealias RawValue = UInt32
+    private var value: UInt32 = 0
+    init(_ value: UInt32) { self.value = value }
+    init(rawValue value: UInt32) { self.value = value }
+    init(nilLiteral: ()) { self.value = 0 }
+    static var allZeros: physicsCategory { return self(0) }
+    static func fromMask(raw: UInt32) -> physicsCategory { return self(raw) }
+    var rawValue: UInt32 { return self.value }
+    
+    static var none: physicsCategory { return self(0) }
+    static var player: physicsCategory { return physicsCategory(1 << 0) }
+    static var enemy: physicsCategory { return physicsCategory(1 << 1) }
+    static var bullet: physicsCategory { return physicsCategory(1 << 2) }
+    static var ground: physicsCategory { return physicsCategory(1 << 3) }
+    static var winTile: physicsCategory { return physicsCategory(1 << 4) }
+    static var coin: physicsCategory { return physicsCategory(1 << 5) }
+    static var spike: physicsCategory { return physicsCategory(1 << 6) }
+    static var doorTile: physicsCategory { return physicsCategory(1 << 7) }
 }
