017070f1a090c612678113028e77192dd1181141
Gabriel Prado Marcolino <marcolino66966@gmail.com>
2015-11-08 19:18:15 -0200

    Area maior de toque dos botões de movimento e pulo

--- TowerUp/Scenes/MissionScene.swift
+++ TowerUp/Scenes/MissionScene.swift
@@ -91,14 +91,14 @@ class MissionScene: GameScene, SKPhysicsContactDelegate {
         switch(self.playerData.configControls.integerValue) {
             
         case 1: //controlsConfig.useButtons.rawValue:
-            self.buttonLeft = Button(textureName: "buttonYellowSquare", text:"<", x:20, y:626, xAlign:.left, yAlign:.down, colorBlendFactor:0.5, top:39, bottom: 39, left:39, right:39)
+            self.buttonLeft = Button(textureName: "buttonYellowSquare", text:"<", x:20, y:626, xAlign:.left, yAlign:.down, colorBlendFactor:0.5, top:Int(Config.currentSceneSize.height/2), bottom: 39, left:39, right:39)
             
             self.addChild(self.buttonLeft)
             
-            self.buttonRight = Button(textureName: "buttonYellowSquare", text:">" ,x:276, y:626, xAlign:.left, yAlign:.down, colorBlendFactor:0.5, top:39, bottom: 39, left:39, right:39)
+            self.buttonRight = Button(textureName: "buttonYellowSquare", text:">" ,x:276, y:626, xAlign:.left, yAlign:.down, colorBlendFactor:0.5, top:Int(Config.currentSceneSize.height/2), bottom: 39, left:39, right:39)
             self.addChild(self.buttonRight)
             
-            self.buttonJump = Button(textureName: "buttonYellow", text:"Jump", x:1014, y:626, xAlign:.right, yAlign:.down, colorBlendFactor:0.5, top:39, bottom: 39, left:39, right:39)
+            self.buttonJump = Button(textureName: "buttonYellow", text:"Jump", x:1014, y:626, xAlign:.right, yAlign:.down, colorBlendFactor:0.5, top:Int(Config.currentSceneSize.height/2), bottom: 39, left:39, right:39)
             
             self.addChild(self.buttonJump)
             break
