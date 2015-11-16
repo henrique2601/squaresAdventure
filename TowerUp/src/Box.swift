02baaff920dd3a8976740208a53be3c010ce5dce
Pablo Henrique Bertaco <pablo@hydragames.com.br>
2015-11-16 16:13:43 -0200

    Ajuste classe box

--- TowerUp/src/Box.swift
+++ TowerUp/src/Box.swift
@@ -21,9 +21,33 @@ class Box: Control {
         super.init(spriteNode: SKSpriteNode(texture: texture), x: Int(position.x), y: Int(position.y), xAlign:.center, yAlign:.center)
         
         self.zPosition = Config.HUDZPosition * 2
+        
+        self.hidden = false
     }
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        Control.touchesBegan(touches)
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        Control.touchesMoved()
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        Control.touchesEnded(touches)
+    }
+    
+    override func touchesCancelled(touches: Set<UITouch>?, withEvent event: UIEvent?) {
+        Control.touchesEnded(touches! as Set<UITouch>)
+    }
+    
+    override var hidden: Bool {
+        didSet {
+            self.userInteractionEnabled = !hidden
+        }
     }
 }
